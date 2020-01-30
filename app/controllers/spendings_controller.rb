60e38cad3c440690c60bb213c13b7b24d7108bae
kunihiko-yonenaga <rabessi.coo@gmail.com>
2020-01-31 01:49:59 +0900

    controller

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,18 +1,18 @@
 class SpendingsController < ApplicationController
   def index
-
+    @spendings = Spending.order(created_at: :asc)
 	end
 
   def show
-
+    @spending = Spending.find(params[:id])
 	end
 
   def new
-
+    @spending = Spending.new()
 	end
 
   def edit
-
+    @spending = Spending.find(params[:id])
 	end
 
   def create
