c968ff0193b33a3a128be945121ccfaa495a0a67
kunihiko-yonenaga <rabessi.coo@gmail.com>
2020-01-31 02:10:46 +0900

    コントローラ追加

--- app/controllers/spendings_controller.rb
+++ app/controllers/spendings_controller.rb
@@ -1,26 +1,37 @@
 class SpendingsController < ApplicationController
   def index
-    @spendings = Spending.order(created_at: :asc)
+      @spendings = Spending.order(created_at: :asc)
 	end
 
   def show
-    @spending = Spending.find(params[:id])
+      @spending = Spending.find(params[:id])
 	end
 
   def new
-    @spending = Spending.new()
+      @spending = Spending.new()
 	end
 
   def edit
-    @spending = Spending.find(params[:id])
+      @spending = Spending.find(params[:id])
 	end
 
   def create
-
+      @spending = Spending.new(params[:spending])
+      if @spending.save
+        redirect_to @spending, notice: "支出科目を登録しました"
+      else
+        render "new"
+      end
 	end
 
   def update
-
+      @spending = Spending.find(params[:id])
+      @spending.assign_attributes(params[:spending])
+      if @spending.save
+        redirect_to @spending, notice: "支出科目を登録しました"
+      else
+        render "new"
+      end
 	end
 
   def destroy
